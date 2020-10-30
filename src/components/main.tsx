b53d8aeab95fa79b78419d8852cf34bfa640e559
freercurse <freerjoseph@yahoo.co.uk>
2020-10-30 13:49:08 +0000

    added secondary file ready for HTML

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,41 +1,39 @@
-import _ from 'lodash';
-import main from './main'
 import * as React from 'react'
 import { Fragment } from 'react'
-import '../styles/main.scss'
+
 
 interface Props {
 
 }
 
 interface State {
-  
+
 
 }
 
-class App extends React.Component<Props, State> {
+class main extends React.Component<Props, State> {
   constructor(props: any) {
     super(props)
     this.state = {
-      
+
     }
   }
 
-  
+
 
   render() {
 
     const authProps = {
-      
+
     }
 
     return (
       <div>
         <Fragment>
-          <main/>
+          Hello World What are you doing there
         </Fragment>
       </div>
     )
   }
 }
-export default App
+export default main
